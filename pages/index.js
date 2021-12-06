315cd40986baa790ede23a469b1da2fbcebb3d04
Hugo <dev.hugoregadas@gmail.com>
2021-12-06 02:07:23 +0100

    updated json

--- pages/index.js
+++ pages/index.js
@@ -56,11 +56,11 @@ export default function Home({ exploreData, cardsData }) {
 }
 
 export async function getStaticProps() {
-  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+  const exploreData = await fetch("https://jsonkeeper.com/b/WFKA").then(
     (res) => res.json()
   );
 
-  const cardsData = await fetch("https://links.papareact.com/zp1").then((res) =>
+  const cardsData = await fetch("https://jsonkeeper.com/b/2WYR").then((res) =>
     res.json()
   );
 
